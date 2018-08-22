d378e4312ee3bf242b96d446bd6ff71ea017628b
sillebarrett <sille@barrett1.dk>
2018-08-22 13:49:05 +0200

    index

---
+++ src/BisonCasino/BisonWWF/mapdata.js
@@ -0,0 +1,18 @@
+var mapdata = {
+
+    bison: {
+        "type": "Feature",
+        "properties": {
+            "image": "img/bison.png",
+            "iconSize": [30, 30],
+            "url": "index.html"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                12.533615,
+                55.675353
+            ]
+        }
+    }
+  };
