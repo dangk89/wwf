6b4353351a571c912a197a9cd1a128f7a9804f84
Lasse HH <ninjalf2@gmail.com>
2018-08-27 19:52:02 +0200

    Add final code

--- src/scripts/data.py
+++ src/scripts/data.py
@@ -1,6 +1,7 @@
-import pandas as pd
-import random
 import json
+import random
+import pandas as pd
+
 from collections import OrderedDict
 from string_to_bytes32 import string_to_bytes32
 
@@ -20,24 +21,8 @@ mapdata = OrderedDict()
 for i in range(50):
 	mapdata["b"+str(i)]= OrderedDict([("type", "Feature"),("properties",OrderedDict([("name",namelist[i]),("gender", random.randint(0,1)), ("age", random.randint(0,20)), ("distance", random.randint(3000,8500)),("max distance traveled", random.randint(6000,9100)), ("total wins", random.randint(0,2)),("image","img/bison.png"),("iconSize",[30,30]),("url","index.html")])),("geometry", OrderedDict([("type", "Point"),("coordinates", [random.uniform(22.35131, 22.535629999999998),random.uniform(45.153040000000004, 45.28548)])]))])
 
-
-
-# Create new constructor input
-"""
-json_data = json.loads(open('mapdata.js').read(), object_pairs_hook=OrderedDict)
-bisonNames = [string_to_bytes32(mapdata[bison]["properties"]["name"]) for bison in mapdata]
-bisonData = [mapdata[bison]["properties"]["distance"] for bison in mapdata]
-
-
-#Save mapdata and bison_data
-with open("mapdata.js", 'w') as f:
-        json.dump(mapdata, f)
-
-df.to_csv("bison_data.csv" ,  index = False)
-"""
-
 # Create constructor input from mapdata file
-json_data = json.loads(open('mapdata.js').read(), object_pairs_hook=OrderedDict)
+json_data = json.loads(open('../../data/mapdata.js').read(), object_pairs_hook=OrderedDict)
 bisonNames = [string_to_bytes32(json_data[bison]["properties"]["name"]) for bison in json_data]
 bisonData = [json_data[bison]["properties"]["distance"] for bison in json_data]
 
